89e38b4001f0b17f7f29d74be2aab2225c0d75f9
khanhpn <khanhpham@bravesoft.vn>
2017-04-21 22:12:13 +0900

    landing page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,12 @@ class HomesController < ApplicationController
 
   def contact
   end
+
+  def download
+    send_file(
+      "#{Rails.root}/public/pdf/Fancy_Nail_Brochure_Mar_8_2017.pdf",
+      filename: "Fancy Nail Brochure - Mar 8, 2017.pdf",
+      type: "application/pdf"
+    )
+  end
 end
