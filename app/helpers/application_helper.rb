72d5cc8c31d2799ba446be1235cbc8b5013a7349
khanhpn <khanhpham@bravesoft.vn>
2017-05-06 08:05:58 +0700

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def default_image?(image)
-    return image_tag(image, size: "140x240") if image.present?
-    image_tag("default_image.jpg", size: "140x240")
+    return image_tag(image, size: "140x190") if image.present?
+    image_tag("default_image.jpg", size: "140x190")
   end
 end
