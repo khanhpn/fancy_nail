38ced0b026c79a43eda81cbd67750b5bbd259e78
khanhpn <khanhpham@bravesoft.vn>
2017-05-08 11:29:43 +0900

    fontawesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+%w(eot svg ttf woff woff2).each do |ext|
+  Rails.application.config.assets.precompile << "fontawesome-webfont.#{ext}"
+end
