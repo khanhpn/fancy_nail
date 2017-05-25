2fbf9a69f2ccf139f68465e81c1ecb7c8a329a05
khanhpn <khanhpham@bravesoft.vn>
2017-05-25 22:28:02 +0900

    generate pdf

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     return image_tag(image, size: "140x190") if image.present?
     image_tag("default_image.jpg", size: "140x190")
   end
+
+  def youtube_video(video_url)
+    video_url = "" if video_url.nil?
+    render :partial => 'layouts/shared/video', locals: { url: video_url }
+  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,6 @@ class Video < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
   paginates_per 10
-  mount_uploader :url_picture, UrlVideoUploader
+  mount_uploader :url_video, UrlVideoUploader
 end
 
